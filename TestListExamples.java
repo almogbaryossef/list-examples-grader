efd0c107ede25d85ecf0979077516a5bde2c923a
almogbaryossef <47935429+almogbaryossef@users.noreply.github.com>
2022-11-15 22:10:28 -0800

    h

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,67 @@
 import static org.junit.Assert.*;
+import java.util.List;
+import java.util.ArrayList;
 import org.junit.*;
 
+// class ListExampless implements StringChecker {
+//   public boolean checkString(String s) {
+//     if(s.length() > 5) {
+//       return true;
+//     }
+//     return false;
+//   }
+// }
+
 public class TestListExamples {
-  // Write your grading tests here!
+
+  @Test
+  public void testFilter() {
+    List<String> lst = new ArrayList<String>();
+    lst.add("hello");
+    lst.add("consequence");
+    lst.add("shannanigans");
+    lst.add("flower");
+    lst.add("code");
+    lst.add("carrot");
+
+    StringChecker sc = new StringChecker() {
+      public boolean checkString(String s) {
+            if(s.length() > 5) {
+              return true;
+            }
+            return false;
+      }
+    };
+      
+    List<String> result = new ArrayList<String>();
+    result.add("consequence");
+    result.add("shannanigans");
+    result.add("flower");
+    result.add("carrot");
+
+    assertArrayEquals(result.toArray(), ListExamples.filter(lst, sc).toArray());
+
+  }
+
+  @Test
+    public void testMerge() {
+        List<String> lst1 = new ArrayList<String>();
+        List<String> lst2 = new ArrayList<String>();
+        lst1.add("a");
+        lst2.add("b");
+        lst1.add("c");
+        lst2.add("d");
+        lst1.add("e");
+        lst2.add("f");
+
+        List<String> result = new ArrayList<>();
+        result.add("a");
+        result.add("b");
+        result.add("c");
+        result.add("d");
+        result.add("e");
+        result.add("f");
+
+        assertArrayEquals(result.toArray(), ListExamples.merge(lst1, lst2).toArray());
+    }
 }
--- grade.sh
+++ grade.sh
@@ -5,3 +5,22 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
+cd student-submission
+
+if [ -f "ListExamples.java" ]
+then
+    echo "file found!"
+else
+    echo "file does not exist!"
+    exit
+fi
+
+cd ..
+cp TestListExamples.java student-submission
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar list-examples-grader/student-submission/ListExamples.java list-examples-grader/student-submission/TestListExamples.java
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore list-examples-grader/student-submission/TestListExamples
+
+#javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar student-submission/ListExamples.java student-submission/TestListExamples.java
+#java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore student-submission/TestListExamples
+
